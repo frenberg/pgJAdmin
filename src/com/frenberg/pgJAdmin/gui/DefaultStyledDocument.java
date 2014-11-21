6fd8e251b82a4a3d5d175dd6d9cd984fe6b6b529
Daniel Frenberg <daniel@digreen.se>
2014-11-21 22:27:09 +0100

    Fixed issue with resource folder not in classpath and also added fallback to default monospace font just in case.

--- src/com/frenberg/pgJAdmin/gui/DefaultStyledDocument.java
+++ src/com/frenberg/pgJAdmin/gui/DefaultStyledDocument.java
@@ -20,208 +20,201 @@ import com.frenberg.pgJAdmin.utils.Keywords;
 
 @SuppressWarnings("serial")
 public class DefaultStyledDocument extends
-		javax.swing.text.DefaultStyledDocument {
+javax.swing.text.DefaultStyledDocument {
 
-	final StyleContext cont = StyleContext.getDefaultStyleContext();
-	final AttributeSet attrKeyword = cont.addAttribute(cont.getEmptySet(),
-			StyleConstants.Foreground, Color.BLUE);
-	final AttributeSet attrDefault = cont.addAttribute(cont.getEmptySet(),
-			StyleConstants.Foreground, Color.BLACK);
-	final AttributeSet attrComment = cont.addAttribute(cont.getEmptySet(),
-			StyleConstants.Foreground, Color.ORANGE);
-	final Pattern keywordPattern = Pattern.compile(Keywords.getRegexString(),
-			Pattern.CASE_INSENSITIVE);
-	final Pattern commentPattern = Pattern.compile("\\-\\-.*");
-	final Pattern blockCommentPattern = Pattern.compile("\\/\\*.*?\\*\\/",
-			Pattern.DOTALL);
-	Font customFont = null;
+    final StyleContext cont = StyleContext.getDefaultStyleContext();
+    final AttributeSet attrKeyword = cont.addAttribute(cont.getEmptySet(),
+            StyleConstants.Foreground, Color.BLUE);
+    final AttributeSet attrDefault = cont.addAttribute(cont.getEmptySet(),
+            StyleConstants.Foreground, Color.BLACK);
+    final AttributeSet attrComment = cont.addAttribute(cont.getEmptySet(),
+            StyleConstants.Foreground, Color.ORANGE);
+    final Pattern keywordPattern = Pattern.compile(Keywords.getRegexString(),
+            Pattern.CASE_INSENSITIVE);
+    final Pattern commentPattern = Pattern.compile("\\-\\-.*");
+    final Pattern blockCommentPattern = Pattern.compile("\\/\\*.*?\\*\\/",
+            Pattern.DOTALL);
+    Font customFont = null;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * javax.swing.text.DefaultStyledDocument#getFont(javax.swing.text.AttributeSet
-	 * )
-	 */
-	@Override
-	public Font getFont(AttributeSet attr) {
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * javax.swing.text.DefaultStyledDocument#getFont(javax.swing.text.AttributeSet
+     * )
+     */
+    @Override
+    public Font getFont(AttributeSet attr) {
 
-		// URL file =
-		// getClass().getClassLoader().getResource("SourceCodePro-Regular.ttf");
+        if (customFont == null) {
+            InputStream in = getClass().getResourceAsStream("/SourceCodePro-Regular.ttf");
+            try {
+                customFont = Font.createFont(Font.TRUETYPE_FONT, in);
+            } catch (FontFormatException | IOException e) {
+                System.err.println(e.getMessage());
+                System.err.println("Fallback to default monospaced font");
+                customFont = new Font("Monospaced", Font.PLAIN, StyleConstants.getFontSize(attr));
+            }
+        }
 
-		if (customFont == null) {
-			try {
-				// InputStream fis = new FileInputStream(new
-				// File(file.toURI()));
-				InputStream fis = getClass().getResourceAsStream(
-						"/SourceCodePro-Regular.ttf");
-				customFont = Font.createFont(Font.TRUETYPE_FONT, fis);
-			} catch (FontFormatException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+        return customFont.deriveFont(Font.PLAIN, StyleConstants.getFontSize(attr));
+    }
 
-		}
-		return customFont.deriveFont(Font.PLAIN,
-				StyleConstants.getFontSize(attr));
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.text.AbstractDocument#remove(int, int)
+     */
+    @Override
+    public void remove(int offset, int length) throws BadLocationException {
+        super.remove(offset, length);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.swing.text.AbstractDocument#remove(int, int)
-	 */
-	@Override
-	public void remove(int offset, int length) throws BadLocationException {
-		super.remove(offset, length);
+        String text = getText(0, getLength());
+        int start = findLastNonWordChar(text, offset);
+        int end = findFirstNonWordChar(text, offset);
 
-		String text = getText(0, getLength());
-		int start = findLastNonWordChar(text, offset);
-		int end = findFirstNonWordChar(text, offset);
+        if (keywordPattern.matcher(text.substring(start, end)).matches()) {
+            setCharacterAttributes(start, end - start, attrKeyword, true);
+        } else {
+            setCharacterAttributes(start, end - start, attrDefault, true);
+        }
 
-		if (keywordPattern.matcher(text.substring(start, end)).matches()) {
-			setCharacterAttributes(start, end - start, attrKeyword, true);
-		} else {
-			setCharacterAttributes(start, end - start, attrDefault, true);
-		}
+        Matcher matcher;
+        matcher = commentPattern.matcher(text);
+        while (matcher.find()) {
+            setCharacterAttributes(matcher.start(),
+                    matcher.end() - matcher.start(), attrComment, true);
+        }
 
-		Matcher matcher;
-		matcher = commentPattern.matcher(text);
-		while (matcher.find()) {
-			setCharacterAttributes(matcher.start(),
-					matcher.end() - matcher.start(), attrComment, true);
-		}
+        matcher = blockCommentPattern.matcher(text);
+        while (matcher.find()) {
+            setCharacterAttributes(matcher.start(),
+                    matcher.end() - matcher.start(), attrComment, true);
+        }
+    }
 
-		matcher = blockCommentPattern.matcher(text);
-		while (matcher.find()) {
-			setCharacterAttributes(matcher.start(),
-					matcher.end() - matcher.start(), attrComment, true);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.swing.text.AbstractDocument#insertString(int,
+     * java.lang.String, javax.swing.text.AttributeSet)
+     */
+    @Override
+    public void insertString(int offset, String string, AttributeSet a)
+            throws BadLocationException {
+        super.insertString(offset, string, attrDefault);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.swing.text.AbstractDocument#insertString(int,
-	 * java.lang.String, javax.swing.text.AttributeSet)
-	 */
-	@Override
-	public void insertString(int offset, String string, AttributeSet a)
-			throws BadLocationException {
-		super.insertString(offset, string, attrDefault);
+        String text = getText(0, getLength());
+        new KeywordSwingWorker(offset, string, text).execute();
 
-		String text = getText(0, getLength());
-		new KeywordSwingWorker(offset, string, text).execute();
+    }
 
-	}
+    class Keyword {
+        int start;
+        int end;
 
-	class Keyword {
-		int start;
-		int end;
+        public Keyword(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
 
-		public Keyword(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
+        public int getStart() {
+            return start;
+        }
 
-		public int getStart() {
-			return start;
-		}
+        public int getEnd() {
+            return end;
+        }
 
-		public int getEnd() {
-			return end;
-		}
+    }
 
-	}
+    class KeywordSwingWorker extends SwingWorker<List<Keyword>, Keyword> {
 
-	class KeywordSwingWorker extends SwingWorker<List<Keyword>, Keyword> {
+        int offset;
+        String string;
+        final String text;
 
-		int offset;
-		String string;
-		final String text;
+        public KeywordSwingWorker(int offset, String string, String text) {
+            this.offset = offset;
+            this.string = string;
+            this.text = text;
+        }
 
-		public KeywordSwingWorker(int offset, String string, String text) {
-			this.offset = offset;
-			this.string = string;
-			this.text = text;
-		}
+        @Override
+        protected List<Keyword> doInBackground() throws Exception {
+            List<Keyword> keywords = new LinkedList<Keyword>();
+            int start = findLastNonWordChar(text, offset);
+            if (start < 0) {
+                start = 0;
+            }
+            int end = findFirstNonWordChar(text, offset + string.length());
 
-		@Override
-		protected List<Keyword> doInBackground() throws Exception {
-			List<Keyword> keywords = new LinkedList<Keyword>();
-			int start = findLastNonWordChar(text, offset);
-			if (start < 0) {
-				start = 0;
-			}
-			int end = findFirstNonWordChar(text, offset + string.length());
+            int wordStart = start;
+            int wordEnd = start;
+            Keyword keyword;
 
-			int wordStart = start;
-			int wordEnd = start;
-			Keyword keyword;
+            while (wordEnd <= end && !isCancelled()) {
+                if (wordEnd == end
+                        || String.valueOf(text.charAt(wordEnd)).matches("\\W")) {
+                    if (keywordPattern.matcher(
+                            text.substring(wordStart, wordEnd)).matches()) {
+                        keyword = new Keyword(wordStart, wordEnd - wordStart);
+                        keywords.add(keyword);
+                        publish(keyword);
+                    }
+                    wordStart = wordEnd;
+                }
+                wordEnd++;
+            }
 
-			while (wordEnd <= end && !isCancelled()) {
-				if (wordEnd == end
-						|| String.valueOf(text.charAt(wordEnd)).matches("\\W")) {
-					if (keywordPattern.matcher(
-							text.substring(wordStart, wordEnd)).matches()) {
-						keyword = new Keyword(wordStart, wordEnd - wordStart);
-						keywords.add(keyword);
-						publish(keyword);
-					}
-					wordStart = wordEnd;
-				}
-				wordEnd++;
-			}
+            return keywords;
+        }
 
-			return keywords;
-		}
+        @Override
+        protected void process(List<Keyword> chunks) {
+            for (Keyword keyword : chunks) {
+                setCharacterAttributes(keyword.getStart(), keyword.getEnd(),
+                        attrKeyword, false);
+            }
+        }
 
-		@Override
-		protected void process(List<Keyword> chunks) {
-			for (Keyword keyword : chunks) {
-				setCharacterAttributes(keyword.getStart(), keyword.getEnd(),
-						attrKeyword, false);
-			}
-		}
+        @Override
+        protected void done() {
+            // TODO Auto-generated method stub
+            super.done();
+            Matcher matcher;
+            matcher = commentPattern.matcher(text);
+            while (matcher.find()) {
+                setCharacterAttributes(matcher.start(),
+                        matcher.end() - matcher.start(), attrComment, true);
+            }
 
-		@Override
-		protected void done() {
-			// TODO Auto-generated method stub
-			super.done();
-			Matcher matcher;
-			matcher = commentPattern.matcher(text);
-			while (matcher.find()) {
-				setCharacterAttributes(matcher.start(),
-						matcher.end() - matcher.start(), attrComment, true);
-			}
+            matcher = blockCommentPattern.matcher(text);
+            while (matcher.find()) {
+                setCharacterAttributes(matcher.start(),
+                        matcher.end() - matcher.start(), attrComment, true);
+            }
+        }
 
-			matcher = blockCommentPattern.matcher(text);
-			while (matcher.find()) {
-				setCharacterAttributes(matcher.start(),
-						matcher.end() - matcher.start(), attrComment, true);
-			}
-		}
+    }
 
-	}
+    private int findLastNonWordChar(String text, int index) {
+        while (--index >= 0) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+        }
+        return index < 0 ? 0 : index;
+    }
 
-	private int findLastNonWordChar(String text, int index) {
-		while (--index >= 0) {
-			if (String.valueOf(text.charAt(index)).matches("\\W")) {
-				break;
-			}
-		}
-		return index < 0 ? 0 : index;
-	}
-
-	private int findFirstNonWordChar(String text, int index) {
-		while (index < text.length()) {
-			if (String.valueOf(text.charAt(index)).matches("\\W")) {
-				break;
-			}
-			index++;
-		}
-		return index;
-	}
+    private int findFirstNonWordChar(String text, int index) {
+        while (index < text.length()) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+            index++;
+        }
+        return index;
+    }
 
 }
